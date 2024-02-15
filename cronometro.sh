6b52606c401528ac582caa41e8bbb36a09752ed7
smartao <smartao@gmail.com>
2024-02-15 15:48:24 -0300

    adicionado validacao na resposta

--- cronometro.sh
+++ cronometro.sh
@@ -1,22 +1,40 @@
 #!/bin/bash
 
-clear
-time=120;
-D="y"
-c=0
-while [ $D = "y" ]
+function MAIN(){
+clear # limpando a tela
+time=2; # tempo padrao
+anwser="y" # resposta padrao
+
+c=0 # contador
+
+
+while [ $anwser != "n" ]
 do
+    validacao=0
     ((c++))
     echo -e "\nIniciando contagem da acao $c"
-    #date
     for (( i=1; i<=$time; i++ ))
     do 
         echo $i 
         sleep 1 
     done
     echo "O tempo acabou!"
-    #date 
-    read -p "comecar novamente? y/n " D
+    while [ $validacao -ne 1 ]
+    do
+        read -p "comecar novamente? y/n " anwser
+        case $anwser in
+        "n") 
+            echo "saindo";
+            validacao=1;;
+        "y") 
+            validacao=1;;
+        *) 
+            echo "digite apenas y/n"
+            validacao=0;;
+        esac
+    done
 done
+} 
 
-
+MAIN
+exit;
