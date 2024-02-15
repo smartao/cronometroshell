c9056df685cd7e33c66896a7f50359ae5d57a413
smartao <smartao@gmail.com>
2024-02-15 16:16:02 -0300

    adicionado funcao e cores

--- cronometro.sh
+++ cronometro.sh
@@ -1,40 +1,52 @@
 #!/bin/bash
 
 function MAIN(){
+CORES # carregando a funcao cores
 clear # limpando a tela
 time=2; # tempo padrao
 anwser="y" # resposta padrao
-
-c=0 # contador
-
+t=0 # contador de tarefas
 
 while [ $anwser != "n" ]
 do
     validacao=0
-    ((c++))
-    echo -e "\nIniciando contagem da acao $c"
+    ((t++))
+    echo -e "\nIniciando contagem da acao: ${CVE}$t${CF}"
     for (( i=1; i<=$time; i++ ))
     do 
-        echo $i 
+        echo -e "${CAM}Contagem: $i ${CF}" 
         sleep 1 
     done
-    echo "O tempo acabou!"
+    echo -e "${CVE}O tempo acabou! ${CF}"
+    read -p "Comecar novamente? y/n " anwser
     while [ $validacao -ne 1 ]
     do
-        read -p "comecar novamente? y/n " anwser
         case $anwser in
-        "n") 
-            echo "saindo";
-            validacao=1;;
-        "y") 
-            validacao=1;;
+        "n")
+            echo -e "${CVD}Saindo ${CF}\n"
+            validacao=1
+            ;;
+        "y")
+            validacao=1
+            ;;
         *) 
-            echo "digite apenas y/n"
-            validacao=0;;
+            echo -en "${CPU}"
+            read -p "Digite apenas y/n! -.-' " anwser
+            echo -e "${CF}"
+            validacao=0
+            ;;
         esac
     done
 done
 } 
+
+function CORES(){
+CVE='\e[0;31m' # Red 
+CAM='\e[0;33m' # Yellow 
+CVD='\e[0;32m' # Verde
+CPU='\e[0;35m' # Roxo
+CF='\e[0m'    # Tag end
+}
 
 MAIN
 exit;
