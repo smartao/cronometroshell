33cf5f77e90b91b123859412baff68b398705e81
smartao <smartao@gmail.com>
2024-02-16 21:31:43 -0300

    adicionado som e modo funny

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # cronometroshell
 Um simples cronometro em shell script
+
+https://img.shields.io/badge/LinuxMint%20-%2021.3%20-%20Green
--- cronometro.sh
+++ cronometro.sh
@@ -2,41 +2,76 @@
 
 function MAIN(){
 
-clear # Limpando a tela
-. variaveis # Importando arquivo de variaveis
+# Diretorio de variaveis
+# Necessario alterar conforme necessidade
+DIR=/home/sergei/Midias/Dev/Shell/cronometroshell
+
+clear
+. ${DIR}/variaveis # Importando arquivo de variaveis
 touch $arqtemp # Criando arquivo temporario caso nao existe
+
 CORES # Executando a funcao cores
-TEMPO $1 # Executando a funcao tempo
+VALIDATEMPO $1 # Executando a funcao valida tempo
 
 while [ $anwser != "n" ] # Validando resposta do usuario
 do
-    validacao=0
+    validacao=0 # setando valor em 0 
     t=$(cat $arqtemp) # Pegando valor da tarefa no arquivo temp
     ((t++)) # Incrementando o numero de tarefas
-    echo -e "\nIniciando contagem da acao: ${CVE} $t ${CF}"
+
+    if [ $funny -eq 1 ]; then # Verificando se o funny esta ativo 
+        cowsay "Iniciando contagem da acao: $t" | $lolcat
+    else
+        echo -e "\nIniciando contagem da acao: ${CVE}$t${CF}"
+    fi
     echo $t > $arqtemp # Gravando incremento de tarefa no arquivo tempo
     for (( i=1; i<=$time; i++ )) # Contagem regressiva timer
     do 
         echo -e "${CAM}Contagem: $i ${CF}" # Mostrando contagem na tela
         sleep 1 
     done
-    echo -e "${CVE}O tempo acabou! ${CF}"
-    read -p "Comecar novamente? y/n " anwser
+    paplay $audio # tocando audio quando terminar a tarefa
+    if [ $funny -eq 1 ]; then # Verificando se o funny esta ativo 
+        cowsay "O tempo acabou! começar novamente? y/n "| $lolcat
+        read anwser
+    else
+        echo -e "${CVE}O tempo acabou! ${CF}"
+        read -p "Comecar novamente? y/n " anwser
+    fi
     while [ $validacao -ne 1 ] # Validador da reposta do usuario
     do
         case $anwser in
         "n")
-            echo -e "${CVD}Saindo ${CF}\n"
+            if [ $funny -eq 1 ]; then 
+                cowsay "By =)"| lolcat
+            else
+                echo -e "${CVD}Saindo${CF}\n"
+            fi
             validacao=1
             ;;
         "y")
             validacao=1
             ;;
         *) 
-            echo -en "${CPU}"
-            read -p "Digite apenas y/n! -.-' " anwser
-            echo -e "${CF}"
-            validacao=0
+            validacao=0 # Desativa o validador
+            if [ $funny -eq 1 ]; then
+                ((e++)) # Contador de erros
+                if [ $e -gt 3 ]; then # caso o numero de erros seja maior que 3 vezes
+                    cowsay -f dragon Dracarys! Digite apenas y/n! | $lolcatdragon
+                    if [ $e -gt 5 ]; then # caso erre mais que 5 vezes
+                        cowsay -d Desisto... | $lolcat
+                        exit;
+                    fi
+                    read anwser 
+                else
+                    cowsay -t Digite apenas y/n! | $lolcat
+                    read anwser 
+               fi
+            else
+                echo -en "${CAM}"
+                read -p "Digite apenas y/n! -.-' " anwser 
+                echo -e "${CF}"
+            fi
             ;;
         esac
     done
@@ -52,10 +87,10 @@ CPU='\e[0;35m' # Roxo
 CF='\e[0m'     # Tag end
 }
 
-function TEMPO(){
+function VALIDATEMPO(){
     time=$1
     if [ -z $time ]; then # validando se existe argumento
-        time=4 # tempo padrao
+        time=$tpadrao # caso nao exista setar tempo padrao
     else # caso exista
         if [[ $time ]] && [ $time -eq $time 2>/dev/null ]; then # validando se é um numero
             echo ok > /dev/null
