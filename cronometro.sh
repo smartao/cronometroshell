63749fab4df4158e44a9dcb2fcf87901de7776db
smartao <smartao@gmail.com>
2024-02-18 22:15:14 -0300

    Adicionado recurso para parar a contagem no meio do cronometro

--- cronometro.sh
+++ cronometro.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 function MAIN(){
-
 # Diretorio de variaveis
 # Necessario alterar conforme necessidade
 DIR=/home/sergei/Midias/Dev/Shell/cronometroshell
 
 clear # Limpando a tela
 . ${DIR}/variaveis # Importando arquivo de variaveis
-touch $arqtemp # Criando arquivo temporario caso nao existe
+
 if [ $fun -eq 1 ]; then 
     APLICACORES # Executando a funcao cores
 fi
@@ -16,42 +15,61 @@ VALIDATEMPO $1 # Executando a funcao valida tempo
 
 while [ $anwser != "n" ] # Validando resposta do usuario
 do
+    # Tela inicial do script
     validacao=0 # definindo valor em 0 
-    t=$(cat $arqtemp) # Pegando valor da tarefa no arquivo temp
     ((t++)) # Incrementando o número de tarefas
     if [ $fun -eq 2 ]; then # Verificando se o fun esta ativo 
         cowsay "Iniciando contagem da acao: $t" | $lolcat
     else
         echo -e "\nIniciando contagem da acao: ${CVE}$t${CF}"
     fi
-    echo $t > $arqtemp # Gravando incremento de tarefa no arquivo tempo
-    for (( i=1; i<=$time; i++ )) # Contagem regressiva na tela
+    # Contagem regressiva da tela
+    for (( i=1; i<=$time; i++ ))
     do 
         ((seed++)) # Incementando valor do seed para alterar a cor
         if [ $fun -eq 2 ]; then 
-            echo -e "Contagem: $i" | lolcat --seed $seed --spread 100
+            echo "Contagem: $i" | lolcat --seed $seed --spread 100
         else
             echo -e "${CAM}Contagem: $i ${CF}" # Mostrando contagem na tela
         fi
-        sleep 1 # 1 segundo da contagem da tela
+        read -n1 -t 1 TECLA # Lendo a tecla e aguardando 1 segundo
+        case "$TECLA" in
+        n) # Caso seja n (next) va para proxima acao
+            ttemp=$time; # Armazendo tempo
+            time=0  # Zerando o time para sair do cronometro
+            if [ $fun -eq 2 ]; then
+                cowsay "Iniciando proxima contagem"| $lolcat
+            else
+                echo -e "${CVD}\nIniciando proxima contagem ${CF}"
+            fi
+        ;;
+        e) # caso seja e (exit) saia do programa
+            SAIDA 
+        ;;
+        esac
     done
-    paplay $audio # tocando audio quando terminar a tarefa
-    if [ $fun -eq 2 ]; then 
-        cowsay "O tempo acabou! começar novamente? y/n "| $lolcat
-        read anwser
-    else
-        echo -e "${CVE}O tempo acabou! ${CF}"
-        read -p "Comecar novamente? y/n " anwser
+    TOCAAUDIO # Funcao para tocar audio
+    # Validando se foi precionado a teclado no meio da contagem
+    if [ -n "$TECLA" ] && [ $TECLA = "n" ]; then # caso sim e seja a tecla n
+        anwser=y # adicionando a reposta padrao
+        seed=22 # Restaurando o valor da seed
+        time=$ttemp # Restaurando o valor do timer
+        sleep $delay # Delay para reiniciar a contagem
+    else # Caso nao, imprimir que o tempo acabou
+        if [ $fun -eq 2 ]; then
+            cowsay "O tempo acabou! começar novamente? y/n "| $lolcat
+            read anwser
+        else
+            echo -e "${CVE}O tempo acabou! ${CF}"
+            read -p "Comecar novamente? y/n " anwser
+        fi
     fi
-    while [ $validacao -ne 1 ] # Validador da reposta do usuario
+    # Validador da reposta do usuario
+    while [ $validacao -ne 1 ]
     do
         case $anwser in
         "n")
-            if [ $fun -eq 2 ]; then 
-                cowsay "By =)"| lolcat
-            else
-                echo -e "${CVD}Saindo${CF}\n"
-            fi
+            SAIDA
             validacao=1
             ;;
         "y")
@@ -65,11 +83,11 @@ do
                     cowsay -f dragon Dracarys! Digite apenas y/n! | $lolcatdragon
                     if [ $e -gt 5 ]; then # caso erre mais que 5 vezes
                         cowsay -d Desisto... | $lolcat
-                        exit;
+                        exit 0;
                     fi
                     read anwser 
                 else
-                    cowsay -t Digite apenas y/n! | $lolcat
+                    cowsay -t "Digite apenas y/n!" | $lolcat
                     read anwser 
                fi
             else
@@ -81,7 +99,6 @@ do
         esac
     done
 done
-rm $arqtemp # Deletando arquivo de contagem de tarefas
 } 
 
 function APLICACORES(){
@@ -104,6 +121,19 @@ function VALIDATEMPO(){
             time=$tpadrao
         fi
     fi
+}
+
+function TOCAAUDIO(){
+    paplay $audio 
+}
+
+function SAIDA(){
+    if [ $fun -eq 2 ]; then 
+        cowsay "Total de acoes:${t} By =) "| lolcat
+    else
+        echo -e "${CVD}\nTotal de acoes:$t Saindo${CF}\n"
+    fi
+    exit 0;
 }
 
 MAIN $1
